2c4b2c8a52d61335ddb55df990977fd83c8fb129
nikbad7 <nikhil.badani@gmail.com>
2018-02-13 09:13:26 +0530

    edit form created

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,22 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
         
       end
+     end
     end
-  end
+    def edit
+  	@portfolio_items = Portfolio.find(params[:id])
+    end
+    def update
+      @portfolio_items = Portfolio.find(params[:id])
+      respond_to do |format|
+      	if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      		format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+        
+        else
+            format.html { render :edit }
+        
+        end
+     end
+    end
 end
 
