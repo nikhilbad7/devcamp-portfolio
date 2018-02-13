4ce50f6436f0094ba5ff18e5de7ba4e2f5b62bcb
nikbad7 <nikhil.badani@gmail.com>
2018-02-14 01:30:35 +0530

    portfolio technologies added

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,9 @@ end
 		)
 end
 puts "9 portfolio created"
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology#{technology}"
+		)
+end
+puts "3 technologies created"
